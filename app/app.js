8da711a0fce1d918f28fc6b5a2f81effb3ab71be
jakerella <jordan@jordankasper.com>
2013-06-28 10:56:08 -0500

    Updated everything to get a basic index working with ejs

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 console.log("Starting server...");
 
-var         pkg = require('./package.json');
+var         pkg = require('../package.json');
         express = require("express"),
            http = require("http"),
          routes = require("./modules/routes.js");
@@ -12,11 +12,11 @@ app.configure(function () {
     process.env["NODE_ENV"] = (process.env["NODE_ENV"] || "production");
     app.set("port", process.env.PORT || 5000);
     app.set("views", __dirname + "/views");
-    app.engine('html', require('ejs'));
+    app.set("view engine", "ejs");
     app.use(express.logger("development"));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.cookieParser("huADnaQnwJXbh3A49VFA"));
+    app.use(express.cookieParser("kTh4Yef7NebvCpoY2ewJU9s"));
     app.use(express.session());
     app.use(app.router);
     app.use(express.static(__dirname + "/public"));
@@ -33,7 +33,7 @@ app.configure("production", function () {
 
 // GETs
 app.get("/", routes.index);
-app.get("/class", routes.hasToken, routes.listClasses);
+//app.get("/class", routes.hasToken, routes.listClasses);
 
 // POSTs
 app.post("/", routes.index);
