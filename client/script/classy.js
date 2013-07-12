743d31b2c2293a165ac8a07ee03e65fd8cb4e265
jakerella <jordan@jordankasper.com>
2013-07-11 19:41:01 -0500

    Modularized the current code

--- client/script/classy.js
+++ client/script/classy.js
@@ -1,22 +1,46 @@
 
 window.CLSY = {
 
-    addCoursesToList: function(data, list) {
-        // variable declaration
-        var listItems = "";
-
-        // input audits
-        if (!data.courses || data.courses.length < 1) { return; }
-        
-        list = $(list);
-        if (!list.length) { return; }
-
-        // build list item HTML...
-        data.courses.forEach(function(course) {
-            listItems += "<li class='course'><a href='courses.html#" + course.id + "'>" + course.name + "</a></li>";
-        });
-        // ...and add it to the DOM
-        list.append(listItems);
+    init: function() {
+        this.courses.addCoursesToList(
+            {
+                "courses": [
+                    {
+                        "id": "js101",
+                        "name": "JavaScript 101",
+                        "description": "Everything you always wanted to know about JavaScript, but were afraid to ask.",
+                        "date": "2013-07-30",
+                        "students": 5,
+                        "max": 10
+                    },
+                    {
+                        "id": "advjs",
+                        "name": "Advanced JavaScript",
+                        "description": "After this course you will know ALL THE JAVASCRIPT... or at least, the fun parts.",
+                        "date": "2013-08-01",
+                        "students": 9,
+                        "max": 10
+                    },
+                    {
+                        "id": "intnode",
+                        "name": "Intermediate Node.js",
+                        "description": "You'll need a basic understanding of Node, but we'll dive into some core features and useful libraries.",
+                        "date": "2013-08-17",
+                        "students": 10,
+                        "max": 10
+                    },
+                    {
+                        "id": "html5",
+                        "name": "HTML5 APIs",
+                        "description": "Multimedia, drag and drop, FileReaders, oh my! You'll learn about these and more.",
+                        "date": "2013-09-01",
+                        "students": 0,
+                        "max": 10
+                    }
+                ]
+            },
+            ".sidebar .upcoming ul"
+        );
     }
 
 };
---
+++ client/script/courses.js
@@ -0,0 +1,28 @@
+
+(function() {
+    
+    // make sure our app is defined
+    window.CLSY = (window.CLSY || {});
+    
+    window.CLSY.courses = {
+        
+        addCoursesToList: function(data, list) {
+            // variable declaration
+            var listItems = "";
+
+            // input audits
+            if (!data.courses || data.courses.length < 1) { return; }
+            
+            list = $(list);
+            if (!list.length) { return; }
+
+            // build list item HTML...
+            data.courses.forEach(function(course) {
+                listItems += "<li class='course'><a href='courses.html#" + course.id + "'>" + course.name + "</a></li>";
+            });
+            // ...and add it to the DOM
+            list.append(listItems);
+        }
+    };
+    
+})();
