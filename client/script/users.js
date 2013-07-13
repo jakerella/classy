e4486ec768d6350ea95ad91a245e7effaeb82768
jakerella <jordan@jordankasper.com>
2013-07-13 15:25:19 -0500

    Added user media exercise

--- client/script/users.js
+++ client/script/users.js
@@ -38,6 +38,62 @@
             });
 
             return user;
+        },
+
+        setupProfilePicCamera: function(container) {
+            var captureMethod, video, canvas, button;
+
+            container = $(container);
+            if (!container.length || container.hasClass("camera-set")) { return; }
+
+            if (navigator.webkitGetUserMedia) {
+                captureMethod = "webkitGetUserMedia";
+            } else if (navigator.mozGetUserMedia) {
+                captureMethod = "mozGetUserMedia";
+            } else {
+                container.html("<p>Sorry, but your browser doesn't support this feature.</p>");
+                return;
+            }
+
+            container.html(
+                "<video height='110' width='150'></video><br />" +
+                "<input type='button' class='snap-pic' value='Take Picture' />" +
+                "<input type='button' class='reset-pic' value='Reset' />" +
+                "<canvas style='display:none;'></canvas>"
+            );
+
+            video = container.find("video");
+            canvas = container.find("canvas");
+            snap = container.find(".snap-pic");
+            reset = container.find(".reset-pic").hide();
+
+            canvas.width(video.width());
+
+            snap.on("click", function() {
+                video.get(0).pause();
+                canvas.get(0).getContext("2d").drawImage(video.get(0), 0, 0, video.width(), video.height());
+                snap.hide();
+                reset.show();
+            });
+            reset.on("click", function() {
+                video.get(0).play();
+                reset.hide();
+                snap.show();
+            });
+
+            navigator[captureMethod](
+                { video: true, audio: true },
+                function(stream) {
+                    video
+                        .prop("src", URL.createObjectURL(stream))
+                        .get(0).play();
+                },
+                function(err) {
+                    container.html("<p>Sorry, but this feature isn't working properly.</p>");
+                }
+            );
+
+            container.addClass("camera-set");
         }
 
     };
